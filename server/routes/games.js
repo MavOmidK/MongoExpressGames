8a0806a2ce617fe20beae05b9e453cb9c21b8863
Omid Khataee <maverickxm2@live.ca>
2017-02-24 07:47:07 -0500

    asd

--- server/routes/games.js
+++ server/routes/games.js
@@ -14,12 +14,12 @@ router.get('/', (req, res, next) => {
   // find all games in the games collection
 
   game.find((err, games) => {
-      
+
     if (err) {
       return console.error(err);
     }
     else {
-      res.render('content/gamelist', {
+      res.render('games/index', {
         title: 'Games',
         games: games
       });
@@ -27,8 +27,57 @@ router.get('/', (req, res, next) => {
   });
 });
 
+/* GET edit - show current game to edit */
+router.get('/:id', (req, res, next) => {
+    // get a ference to the ID of the game to edit
+    let id = req.params.id;
+
+    //find the game to edit by it's ID in the games collection    
+    game.findById(id, (err, games) =>{
+
+        if(err){
+            console.error(err);
+            res.end(error);
+        }
+        else{
+            // show the edit view
+            res.render('games/details', {
+              title: 'Games',
+              games: games
+            });
+        }
+    });
+});
+
+/* GET edit - proccess the game to edit */
+router.post('/:id', (req, res, next) => {
+    // get a ference to the ID of the game to edit
+    let id = req.params.id;
+
+    // create a new games object to hold the changes
+    let games = new game({
+      "_id": id,
+      "name": req.body.name,
+      "cost": req.body.cost,
+      "rating": req.body.rating
+    });    
+    
+    game.update({ _id: id}, games, (err) => {
+      if (err){
+        console.log(err);
+        res.end(error);
+      }
+      else{
+        res.redirect('/games');
+      }
+    });
+});
 
 
+
+
+
+module.exports = router;
 
 
 
