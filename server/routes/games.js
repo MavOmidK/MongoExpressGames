077c81f892df880b0b85f3f767b6c16c9b2b35c5
Omid Khataee <maverickxm2@live.ca>
2017-02-23 22:09:34 -0500

    asd

--- server/routes/games.js
+++ server/routes/games.js
@@ -9,7 +9,23 @@ let mongoose = require('mongoose');
 // create the game object - represents a document in the games collection
 let game = require('../models/games');
 
+/* GET games page. */
+router.get('/', (req, res, next) => {
+  // find all games in the games collection
 
+  game.find((err, games) => {
+      
+    if (err) {
+      return console.error(err);
+    }
+    else {
+      res.render('content/gamelist', {
+        title: 'Games',
+        games: games
+      });
+    }
+  });
+});
 
 
 
--- server/routes/index.js
+++ server/routes/index.js
@@ -14,40 +14,6 @@ router.get('/', (req, res, next) => {
    });
 });
 
-/* GET about page. */
-router.get('/gamelist', (req, res, next) => {
-  // find all games in the games collection
-  game.find((err, games) => {
-    if (err) {
-      return console.error(err);
-    }
-    else {
-      res.render('content/gamelist', {
-        title: 'Games List',
-        games: games
-      });
-    }
-
-  });
-
-
-
-});
-
-/* GET products page. */
-router.get('/projects', (req, res, next) => {
-  res.render('content/projects', {
-    title: 'Projects'
-   });
-});
-
-/* GET services page. */
-router.get('/services', (req, res, next) => {
-  res.render('content/services', {
-    title: 'Services'
-   });
-});
-
 /* GET contact page. */
 router.get('/contact', (req, res, next) => {
   res.render('content/contact', {
