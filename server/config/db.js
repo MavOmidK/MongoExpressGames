9a40ddf87b6ad30a106c52808023d3d09d8fce1f
Omid Khataee <maverickxm2@live.ca>
2017-02-23 21:51:39 -0500

    asd

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 module.exports = {
     // Local Mongodb
-    "URI" : "mongodb://localhost/test"
+    //"URI" : "mongodb://localhost/test"
     // MLab mongodb
-    //"URI" : "mongodb://maverickxm2:Meccas11!@ds044679.mlab.com:44679/videogames"
+    "URI" : "mongodb://maverickxm2:Meccas11!@ds044679.mlab.com:44679/videogames"
 }
 
